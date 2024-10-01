80d75884b3c96cc4353cdeca1aec05b8af0dedc5
Kirill <Kirill@Kirill.com>
2024-10-01 10:47:29 +0500

    python module 2-3

---
+++ 2_1_basic.py
@@ -0,0 +1,21 @@
+num = 3
+print(type(num))
+
+num = 3.14
+print(type(num))
+
+print(abs(-3))
+
+print(round(3.75))
+
+print(round(3.75, 1))
+
+num1 = '100'
+num2 = '200'
+
+print(num1 + num2)
+
+num1 = '100'
+num2 = '200'
+
+print(int(num1) + int(num2))
